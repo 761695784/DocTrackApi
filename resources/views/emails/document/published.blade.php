4aad626674a6b7e76bd63b1514bddd9e2faa05c6
Malcom <malcom70976@gmail.com>
2024-10-09 16:28:05 +0000

    modification du template du mail

--- resources/views/emails/document/published.blade.php
+++ resources/views/emails/document/published.blade.php
@@ -5,7 +5,7 @@ Bonjour,
 
 Suite à votre déclaration de perte, nous avons le plaisir de vous informer que le document portant le prénom **{{ $document->OwnerFirstName }}** et le nom **{{ $document->OwnerLastName }}** a été récemment publié. Il pourrait correspondre à votre déclaration de perte.
 
-Pour toute question ou pour obtenir plus d'informations, nous vous encourageons à contacter directement l'auteur de la publication. Voici son numéro de téléphone : **{{ $Phone }}**.
+Nous vous encourageons à clicquer sur le lien ci dessous pour suivre le processus de la demande de restitution .
 
 @component('mail::button', ['url' => $documentUrl]) <!-- Assurez-vous que $documentUrl est bien passé -->
 Voir la publication
