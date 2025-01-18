03a8b43011d0bff572f9ab4f8035bc94de7db329
Malcom <malcom70976@gmail.com>
2025-01-18 15:01:40 +0100

    nouveau fichier insomnia

--- app/Http/Controllers/Annotations/AuthentificationAnnotationController.php
+++ app/Http/Controllers/Annotations/AuthentificationAnnotationController.php
@@ -26,6 +26,63 @@ namespace App\Http\Controllers\Annotations ;
  *
 
  * @OA\POST(
+ *     path="/api/reset-password",
+ *     summary="Reinitialisation password",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="201", description="Created successfully"),
+ * @OA\Response(response="400", description="Bad Request"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="email", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Authentification"},
+*),
+
+
+ * @OA\GET(
+ *     path="/api/forgot-password",
+ *     summary="Reinitialisation lien",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="email", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Authentification"},
+*),
+
+
+ * @OA\POST(
  *     path="/api/register",
  *     summary="Register Simple User",
  *     description="",
